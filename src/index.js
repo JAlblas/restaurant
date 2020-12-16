0cc49c1fa78d6d20348f1178e908aafe05d9f94d
Jasper Alblas <just_jasper@hotmail.com>
2020-12-16 11:58:54 +0100

    Webpack config setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("THIS IS WORKING!");
+console.log("THIS IS WORKING AGAINs!");
+
+import './style.css';
+import Logo from './cheese.jpg';
+ 
+ function component() {
+   //let content = document.querySelector('#content');
+   const element = document.createElement('div');
+ 
+   const header = document.createElement('h1');
+   header.textContent = "Kaas Imperium";
+   element.appendChild(header);
+
+   // Add the image to our existing div.
+   const myLogo = new Image();
+   myLogo.src = Logo;
+
+   element.appendChild(myLogo);
+
+   return element;
+ }
+ 
+ document.body.appendChild(component());
