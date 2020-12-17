115f7679931802436e044fbcd35980f92c75151f
Jasper Alblas <just_jasper@hotmail.com>
2020-12-17 14:20:41 +0100

    done for now

--- src/menu.js
+++ src/menu.js
@@ -6,8 +6,29 @@ function component() {
     const header = document.createElement('h1');
     header.textContent = "Kaas Imperium Menu";
     element.appendChild(header);
+
+    const table = tableCreate();
+    element.appendChild(table);
  
     return element;
+  }
+
+  function tableCreate() {
+    var tbl = document.createElement('table');
+    tbl.style.width = '100%';
+    tbl.setAttribute('border', '1');
+    var tbdy = document.createElement('tbody');
+    for (var i = 0; i < 3; i++) {
+      var tr = document.createElement('tr');
+      for (var j = 0; j < 2; j++) {
+          var td = document.createElement('td');
+          td.textContent = "Burger";
+          tr.appendChild(td)
+      }
+      tbdy.appendChild(tr);
+    }
+    tbl.appendChild(tbdy);
+    return tbl;
   }
 
   export default component;
